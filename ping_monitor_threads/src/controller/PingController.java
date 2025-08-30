8430db49cbeab6ffca9f4f889dd52a28cb36685d
Zang_23 <thiagoaraujo20005@gmail.com>
2025-08-29 21:32:29 -0300

    Terminando o projeto, testado em linux e windows

--- ping_monitor_threads/src/controller/PingController.java
+++ ping_monitor_threads/src/controller/PingController.java
@@ -22,21 +22,30 @@ public class PingController {
     		InputStreamReader leitor = new InputStreamReader(fluxo);
     		BufferedReader buffer = new BufferedReader(leitor);
     		String linha = buffer.readLine();
+    		double soma = 0;
     		while(linha != null) {
     			if(linha.contains("time=")) {
     				String[] vetPing = linha.split(" ");
-    				for(String i: vetPing) {
-    					System.out.println(i);
-    				}
+    				String[] vetTempo = vetPing[vetPing.length-2].split("=");
+    				System.out.println("Site: " + nomeSite + " - Tempo ping: " + vetTempo[1]);
+    				
+    				soma += Double.parseDouble(vetTempo[1]);
+    				
     			}
     			
     			linha = buffer.readLine();
     		}
+    		double media = soma / 10;
+    		System.out.println("Média " + nomeSite + ": " + media + "ms");
     		
     	}catch(Exception e ) {
     		System.err.println(e.getMessage());
     	}
 
+    }
+    
+    public boolean isLinux() {
+    	return System.getProperty("os.name").toLowerCase().contains("linux");
     }
     
    
--- ping_monitor_threads/src/view/Main.java
+++ ping_monitor_threads/src/view/Main.java
@@ -1,13 +1,27 @@
 package view;
 
+import controller.PingController;
 import controller.ThreadPings;
 
 public class Main {
     public static void main(String[] args) {
         
-    	ThreadPings tp = new ThreadPings("google.com.br", "google");
+    	PingController pc = new PingController();
     	
-    	tp.start();
+    	if(pc.isLinux()) {
+    		ThreadPings tp1 = new ThreadPings("google.com.br", "google");	
+    		ThreadPings tp2 = new ThreadPings("terra.com.br", "terra");
+    		ThreadPings tp3 = new ThreadPings("uol.com.br", "uol");
+    		
+    		tp1.start();
+    		tp2.start();
+    		tp3.start();
+    		
+    	}else {
+    		System.err.println("Esse sistema operacional não suporta essa aplicação.");
+    	}
+    	
+    	
     	
     }
 }
